648314038b40b54de1c7f8822a9dd5c27f70ecdb
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-27 05:00:08 +0300

    updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,9 +4,6 @@
  * @return {Array} An array containing task and task2 values.
  */
 export default function taskBlock(trueOrFalse) {
-  const innerTask = false;
-  const innerTask2 = true;
-
   if (trueOrFalse) {
     const innerTask = true;
     const innerTask2 = false;
