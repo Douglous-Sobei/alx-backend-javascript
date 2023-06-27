8b55f07a62812144cf7dcc690df8594a13c5d230
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-27 07:01:07 +0300

    new update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -8,8 +8,6 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
