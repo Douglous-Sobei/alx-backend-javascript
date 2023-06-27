1dcb2fc7cd02d81f6984533450b446211be3c196
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-27 06:03:14 +0300

    task 13

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,28 @@
+/**
+ * Creates an iterator object to iterate through every employee in every department.
+ * @param {Object} report - The report object created with createReportObject.
+ * @return {Object} An iterator object to iterate through employees.
+ */
+export default function createIteratorObject(report) {
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+  const departments = Object.keys(report.allEmployees);
+  const employees = Object.values(report.allEmployees).flatMap((list) => list);
+
+  return {
+    next() {
+      if (currentEmployeeIndex >= employees.length) {
+        if (currentDepartmentIndex >= departments.length) {
+          return {done: true};
+        }
+        currentDepartmentIndex++;
+        currentEmployeeIndex = 0;
+      }
+
+      const employee = employees[currentEmployeeIndex];
+      currentEmployeeIndex++;
+
+      return {value: employee, done: false};
+    },
+  };
+}
