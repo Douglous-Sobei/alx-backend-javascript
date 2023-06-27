7dd9c022d723c87e945475cae2ca020726eabd37
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-27 04:31:50 +0300

    updated

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,27 @@
+/**
+ * Represents the taskFirst function.
+ * @return {string} The task string.
+ */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+/**
+   * Represents the getLast function.
+   * @return {string} The string indicating "is okay".
+   */
+export function getLast() {
+  return ' is okay';
+}
+
+/**
+   * Represents the taskNext function.
+   * @return {string} The combination string.
+   */
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
