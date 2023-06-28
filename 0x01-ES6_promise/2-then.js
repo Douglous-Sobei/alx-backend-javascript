7f5b1e27d27b561587387815ef8946b7169abffc
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 00:24:18 +0300

    task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,16 @@
 /**
  * Handles the response from the API promise.
  * @param {Promise} promise - The promise from the API.
- * @return {Promise} A promise that resolves to an object with the status and body properties.
+ * @returns {Promise} A promise that resolves to an object with the status and body properties.
  */
 function handleResponseFromAPI(promise) {
   return promise
-      .then(() => ({
-        status: 200,
-        body: 'success',
-      }))
-      .catch(() => Error())
-      .finally(() => console.log('Got a response from the API'));
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
+
 export default handleResponseFromAPI;
