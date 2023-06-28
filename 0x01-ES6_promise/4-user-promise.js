fed4ce0dbb5ad9264a45c5b133f99792cf706af9
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 00:45:32 +0300

    task 4

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -5,8 +5,10 @@
  * @return {Promise} A promise that resolves with an object containing the user's first name and last name.
  */
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+  return Promise.resolve(
+      {
+        firstName,
+        lastName,
+      },
+  );
 }
