97f733c1f4760dd4f11657f6e088fac586a34287
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 00:14:15 +0300

    task 2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,8 @@
+/**
+ * Returns a promise that resolves or rejects based on the given boolean parameter.
+ * @param {boolean} success - Indicates whether the response should be resolved or rejected.
+ * @return {Promise} A promise that resolves or rejects based on the success parameter.
+ */
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
@@ -6,9 +11,7 @@ export default function getFullResponseFromAPI(success) {
         body: 'Success',
       });
     } else {
-      reject(new Error(
-          'The fake API is not working currently',
-      ));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
