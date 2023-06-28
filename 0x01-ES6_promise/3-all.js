ed42014725cf499685bccdad452322a2593a2e1d
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 00:38:03 +0300

    task 4

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,14 @@
-import { uploadPhoto, createUser } from './utils';
+import {uploadPhoto, createUser} from './utils';
 
 /**
  * Handles the profile signup process.
- * @returns {Promise} A promise that resolves when the profile signup is completed.
+ * @return {Promise} A promise that resolves when the profile signup is completed.
  */
 export default function handleProfileSignup() {
   return Promise
-    .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+      .all([uploadPhoto(), createUser()])
+      .then((res) => {
+        console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+      })
+      .catch(() => console.log('Signup system offline'));
 }
