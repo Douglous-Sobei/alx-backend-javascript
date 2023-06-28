1acbf8f1832c7431fcee6c6b02b34c508996ac92
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 00:59:42 +0300

    task 6

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,10 +1,18 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 /**
  * Uploads a photo with the provided filename.
  * @param {string} filename - The name of the photo file.
  * @return {Promise} A promise that rejects with an error indicating that the photo cannot be processed.
  */
-export default function uploadPhoto(filename) {
-  return Promise.reject(
-      new Error(`${filename} cannot be processed`),
-  );
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+      .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+      .then((res) => (
+        res.map((o) => ({
+          status: o.status,
+          value: o.status === 'fulfilled' ? o.value : String(o.reason),
+        }))
+      ));
 }
