d25ccb69196b13f9adf003f6140b63f6979fbc11
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-29 01:03:36 +0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 /**
  * Handles the profile signup process by calling signUpUser and uploadPhoto functions.
@@ -8,13 +8,13 @@ import uploadPhoto from './5-photo-reject.js';
  * @param {string} fileName - The name of the photo file.
  * @return {Promise<Array>} A promise that resolves with an array of objects containing the status and value/error of each promise.
  */
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise])
-      .then((results) => results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+      .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+      .then((results) => (
+        results.map((result) => ({
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : String(result.reason),
+        }))
+      ));
 }
