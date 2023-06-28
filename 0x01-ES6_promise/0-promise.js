1984a3e413b8b9fad10b951e4c80b1f8a7cf8082
Douglous Sobei <douglousmangoyi@gmail.com>
2023-06-28 23:23:21 +0300

    task 0

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+/**
+ * Returns a Promise that represents a response from an API.
+ * @return {Promise} A Promise that resolves to the API response.
+*/
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
+}
