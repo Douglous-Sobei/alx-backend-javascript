78c0c88f1e5887e3890876e6f108e6021fe35450
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-05 01:38:26 +0300

    jsdoc

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,33 +2,21 @@
  * Returns an array of students
  * @returns {Array} Array of students objects.
  */
-export default function getListStudents() {
-  /**
-   * Represents a student
-   * @typedef {Object} Student
-   * @property {Number} id - Student ID.
-   * @property {String} firstName - Student Name.
-   * @property {String} location - Student location.
-   */
-
-  /**
-   * Array of student objects
-   * @type {Array<Student>}
-   */
+export default function getListStudents () {
   const student1 = {
     id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco",
-  };
+    firstName: 'Guillaume',
+    location: 'San Francisco'
+  }
   const student2 = {
     id: 2,
-    firstName: "James",
-    location: "Columbia",
-  };
+    firstName: 'James',
+    location: 'Columbia'
+  }
   const student3 = {
     id: 5,
-    firstName: "Serena",
-    location: "San Francisco",
-  };
-  return [student1, student2, student3];
+    firstName: 'Serena',
+    location: 'San Francisco'
+  }
+  return [student1, student2, student3]
 }
