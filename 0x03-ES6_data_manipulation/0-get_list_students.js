d97a6941a00278d5bed95f35de518b5f536c87c1
Douglous Sobei <douglousmangoyi@gmail.com>
2023-07-05 01:28:37 +0300

    updated

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -15,10 +15,20 @@ function getListStudents() {
    * Array of student objects
    * @type {Array<Student>}
    */
-  const students = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 3, firstName: "Serena", location: "San Francisco" },
-  ];
-  return students;
+  const student1 = {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  };
+  const student2 = {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  };
+  const student3 = {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  };
+  return [student1, student2, student3];
 }
